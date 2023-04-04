ecd3c66ad285d5315778e13bb16e0b6deeac8dfe
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-04 13:51:25 +0900

    #11. Linting

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -124,7 +124,9 @@ class BaseRecipeAttrViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        assigned_only = bool(int(self.request.query_params.get("assigned_only", 0)))
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0)),
+        )
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
