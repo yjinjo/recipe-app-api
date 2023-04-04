91473d85d228f8d1aed388349f41d97ab428d94a
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-04 11:04:06 +0900

    #10. Implement image API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -408,7 +408,7 @@ class ImageUploadTests(TestCase):
 
     def test_upload_image(self):
         """Test uploading an image to a recipe."""
-        url = image_upload_url(self.recipe._check_id_field())
+        url = image_upload_url(self.recipe.id)
         with tempfile.NamedTemporaryFile(suffix=".jpg") as image_file:
             img = Image.new("RGB", (10, 10))
             img.save(image_file, format="JPEG")
@@ -423,7 +423,7 @@ class ImageUploadTests(TestCase):
 
     def test_upload_image_bad_request(self):
         """Test uploading invalid image."""
-        url = image_upload_url(self.recipe._check_id_field)
+        url = image_upload_url(self.recipe.id)
         payload = {"image": "notanimage"}
         res = self.client.post(url, payload, format="multipart")
 
